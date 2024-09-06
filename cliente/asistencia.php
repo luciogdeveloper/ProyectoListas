5459fe8851ecbbc9763d4111801ab108ef0b4f58
luciogayoso <luciogaoyo@gmail.com>
2024-09-06 15:19:59 -0300

    logo del header

--- cliente/asistencia.php
+++ cliente/asistencia.php
@@ -5,15 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Turno Noche Alumnos</title>
     <link rel="shortcut icon" href="../img/icono.jfif" style="border-radius:5px" type="image/x-icon">
+    <link rel="stylesheet" href="../css/index.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/asistencia.css">
   </head>
 <body>
 <div class="container">
-<h3 class="selectTExt" style="text-align: end">
-  Liceo 8 Turno Noche
-  <small class="text-muted">Alumnos</small>
-</h3>
+<br>
+  <div class="logo"">
+  <img src="../img/logoLiceo.png" alt="">
+  </div><br><br>
 <ul class="nav nav-tabs">
   <li class="nav-item">
     <a class="nav-link" href="../index.php">Inicio</a>
--- index.php
+++ index.php
@@ -5,14 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Turno Noche Alumnos</title>
     <link rel="shortcut icon" href="img/icono.jfif" style="border-radius:5px" type="image/x-icon">
+    <link rel="stylesheet" href="css/index.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
 <div class="container">
-<h3 style="text-align: end">
-  Liceo 8 Turno Noche
-  <small class="text-muted">Alumnos</small>
-</h3>
+  <br>
+  <div class="logo"">
+  <img src="img/logoLiceo.png" alt="">
+  </div><br><br>
 <ul class="nav nav-tabs">
   <li class="nav-item">
     <a class="nav-link active" aria-current="page" href="#">Inicio</a>
