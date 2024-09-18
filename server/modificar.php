f6581b4e657809ee38d5a244be7d665169fa4fda
luciogayoso <luciogaoyo@gmail.com>
2024-09-18 12:00:24 -0300

    Pagina de trancicion de modificar

--- server/modificar.php
+++ server/modificar.php
@@ -11,7 +11,35 @@ $materia = $_POST["materia"];
 include("../modelo/alumno.php");
 $resul = modificar( $id,$apellido,$nombre, $dni,$mail,$telefono,$curso, $materia);
 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Turno Noche Alumnos</title>
+    <link rel="shortcut icon" href="../img/icono.jfif" style="border-radius:5px" type="image/x-icon">
+    <link href="../css/bootstrap/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <div style="display:flex;justify-content:center;align-items:center; height: 100vh;">
+    <div class="spinner-grow text-primary" role="status">
+  <span class="sr-only"></span>
+</div>
+<div class="spinner-grow text-secondary" role="status">
+  <span class="sr-only"></span>
+</div>
+<div class="spinner-grow text-success" role="status">
+  <span class="sr-only"></span>
+</div>
+</div>
+
+<?php
 header("refresh:5;url=../index.php");
 exit();
-
 ?>
+
+<script src="../css/bootstrap/js/bootstrap.js"></script>
+</body>
+</html>
